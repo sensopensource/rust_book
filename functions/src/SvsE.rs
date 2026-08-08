f9a9ba0bba26ce29649a6ff08b8c82cb71cb56b4
sensopensource <souidi.ayoub.22@gmail.com>
2026-08-08 12:45:38 +0100

    chap 3still: 3.3 functions,very interesting (statements,expressions,return value,unit'()'

---
+++ functions/src/SvsE.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = five();
+        
+    println!{"value of x is {x}"};
+
+}
+
+
+fn five() -> i32 {
+    5
+}
---
+++ functions/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(7);
+}
+
+fn another_function(x: i32){
+    println!("value of x : {x}");
+}
+
