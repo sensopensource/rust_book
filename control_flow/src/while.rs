c16d648f34e2090497fdbbb9b734ca14d7fdf55f
sensopensource <souidi.ayoub.22@gmail.com>
2026-08-10 20:53:04 +0100

    chap 3: finito ownership is next

---
+++ control_flow/src/while.rs
@@ -0,0 +1,22 @@
+fn main() {
+
+    let mut number = 3;
+
+    while number !=0 {
+        println!("{number}");
+
+        number -=1;
+    }
+    println!("LIFTOFF!");
+
+
+
+  let a = [10,20,30,40,50];
+
+  let mut index = 0;
+
+  while index < 5 {
+      println!("value = {}",a[index]);
+      index +=1;
+  }
+}
