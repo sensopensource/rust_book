13930d59e82a89e273d3b5c1ee47dac1d9051295
sensopensource <souidi.ayoub.22@gmail.com>
2026-08-14 07:01:56 +0100

    chap 4: 4.2 references et borrowing,slice type is next

---
+++ references/src/dangle.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let reference_to_nothing = dangle();
+
+}
+
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
+}
---
+++ references/src/main2.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{s}");
+}
+
+fn change(s: &mut String) {
+
+    s.push_str(", world");
+
+}
